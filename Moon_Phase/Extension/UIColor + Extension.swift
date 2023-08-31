0e25b353f3397ae1f1dd4b18a1c888a03716e53c
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-08-31 15:33:21 -0300

    init

---
+++ Moon_Phase/Extension/UIColor + Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor + Extension.swift
+//  Moon_Phase
+//
+//  Created by Jadië Oliveira on 31/08/23.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let primaryColor = UIColor(red: 88/255, green: 11/255, blue: 248/255, alpha: 1.0)
+    static let secundaryColor = UIColor(red: 255/255, green: 246/255, blue: 238/255, alpha: 1.0)
+}
---
+++ Moon_Phase/Home/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  Moon_Phase
+//
+//  Created by Jadië Oliveira on 31/08/23.
+//
+
+import UIKit
+
+class HomeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
